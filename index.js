6b71a00c8f0f46252673022c93343474d4a71740
k0s02oi <krishnamurthy.sannidhi@walmart.com>
2022-02-02 21:58:02 -0600

    created http server and added test http to test api requests.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello World!");
+const http = require('http');
+
+const httpServer = http.createServer((req, res)=>{
+    res.end("Hello World!");
+});
+
+httpServer.listen(12345, ()=>{
+    console.log("this server listening on port 12345");
+})
